f9fdd00e88c10a2cf858fe6bc115b39f413b2483
maheshpareta <maheshpareta4@gmail.com>
2023-11-04 18:38:50 +0530

    First Commit

---
+++ C language/hi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main(){
+	int num,num1,add;
+	
+	printf("Enter your number : ");
+	scanf("%d",&num);
+	printf("Enter your second num : ");
+	scanf("%d",&num1);
+	add=num+num1;
+	printf("This num %d",add);
+}
